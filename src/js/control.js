911afa7a77bf728b066c355b71e7f8c040a892d2
bonkerman <markis.markis@gmail.com>
2020-02-09 23:15:25 +0200

    Added forward function and changed default source

--- src/js/control.js
+++ src/js/control.js
@@ -2,7 +2,7 @@ var video = null;
 
 $(document).ready(function () {
     video = videojs('video');
-    video.src("https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4");
+    video.src("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
     // video.play();
     console.log("ready!");
     $("#play").click(function () {
@@ -16,6 +16,7 @@ $(document).ready(function () {
         video.reset();
     });
     $("#fwd5").click(function () {
+        video.currentTime(video.currentTime() + 5);
     });
     $("#bck5").click(function () {
     });
