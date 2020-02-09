0c0124893b7d1b3f8320da2129c976a63c4352d1
bonkerman <markis.markis@gmail.com>
2020-02-09 23:01:58 +0200

    Added button handlers

---
+++ src/js/control.js
@@ -0,0 +1,23 @@
+var video = null;
+
+$(document).ready(function () {
+    video = videojs('video');
+    video.src("https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4");
+    // video.play();
+    console.log("ready!");
+    $("#play").click(function () {
+        console.log("Pushed play");
+        video.play();
+    });
+    $("#pause").click(function () {
+    });
+    $("#stop").click(function () {
+    });
+    $("#fwd5").click(function () {
+    });
+    $("#bck5").click(function () {
+    });
+    $("#load").click(function () {
+    });
+});
+
